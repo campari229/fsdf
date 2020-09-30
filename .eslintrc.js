0273ef66f025f8d4c057d5a53b614e7ae4f96be0
Korol Olexandr <63782107+campari229@users.noreply.github.com>
2020-09-30 19:10:59 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: '@mate-academy/eslint-config',
+  parserOptions: {
+    sourceType: 'module',
+  },
+
+  rules: {
+    strict: 0,
+  },
+  env: {
+    jest: true,
+  },
+  plugins: ['jest'],
+};
